e5ebcd49e80741c509b58992ce57159ac92751d3
Arkar Aung <arica.aung@gmail.com>
2021-07-19 22:26:32 +0800

    Add initial set of completed exercies

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -7,12 +7,12 @@
 // Execute the command `rustlings hint vec2` if you need
 // hints.
 
-// I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
+        *i = *i *  2;       //iter_mut returns address of elements in vector
+                            //so, we should dereference it first, do our 
+                            //multiplication, and then update 
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
